bbcbd54224b7feb1062bdab3af09783b2d2c03d3
Sherwood Callaway <shcallaway@gmail.com>
2017-01-04 11:37:19 -0500

    Game component handles keypress events

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Game />
     );
+  }
+}
+
+// var DIRECTIONS = ['left', 'right', 'up', 'down']
+var KEYCODES = [37, 38, 39, 40]
+
+class Game extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      head: [0, 0]
+    }
+  }
+
+  move(direction) {
+    var head = this.state.head;
+    var x = head[0], y = head[1];
+
+    switch (direction) {
+      case 37: x--; break; // left
+      case 38: x++; break; // right
+      case 39: y++; break; // up
+      case 40: y--; break; // down
+      default: return;
+    }
+
+    console.log('x: ' + x + ', y: ' + y);
+    this.setState({head: [x, y]});
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", e => this.handleKeyPress(e), false);
+  }
+
+  handleKeyPress(e) {
+    var directionalKey = KEYCODES.indexOf(e.keyCode) == -1 ? false : true;
+
+    if (directionalKey) {
+      this.move(e.keyCode)
+    }
+  }
+
+  render() {
+    return <div className="Game" />
   }
 }
 
